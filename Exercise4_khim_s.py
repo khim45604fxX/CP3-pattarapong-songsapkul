42d60ed3bbb6cf4421c5864eb1775a6766b5a78b
khim45604fxX <58694387+khim45604fxX@users.noreply.github.com>
2019-12-19 16:31:36 +0700

    Exercise4_khim_s.py

---
+++ Exercise4_khim_s.py
@@ -0,0 +1,10 @@
+ FE = 80 # foundation English
+ GB = 50 # general business
+ ICS = 60.5 # Introducing to computer systems
+ CP = 40 # computer programming 
+ print("---------------your score-----------------------")
+ print("foundation english :", FE, "score")
+ print("general business :" GB, "score")
+ print("introduction to computer system :", ICS,"score")
+ print("computer programming :", CP,"score")
+ print("------------------------------------------------")
